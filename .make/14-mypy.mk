5823d42161579dc0add02a4922765b11ffcb1ac6
Wouter Vanden Hove <wouter@libranet.eu>
2023-10-07 14:16:49 +0200

    update

--- .make/14-mypy.mk
+++ .make/14-mypy.mk
@@ -7,13 +7,15 @@ mypy-which:
 
 .PHONY: mypy ## run mypy on python-files
 mypy: mypy-which
-	mypy src tests
+	# mypy src tests
+	mypy src
 
 
 .PHONY: mypy-report ## run mypy with html-reporting
 mypy-report: mypy-which
 	@ mkdir -p var/html/mypy/
-	mypy src tests --html-report var/html/mypy/
+	# mypy src tests --html-report var/html/mypy/
+	mypy src --html-report var/html/mypy/
 
 
 .PHONY: mypy-html ## alias for mypy-report
